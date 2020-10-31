a73615d51c2ad438b09c2fc21cde7455b7e88102
dvashist <dvashist@tamu.edu>
2020-10-31 14:37:27 -0500

    Modified Test cases

--- LinkedList/Test.cpp
+++ LinkedList/Test.cpp
@@ -4,29 +4,34 @@
 using namespace std;
 
 
-int NodeTest(){
+void NodeTest(){
     Node a;
     Node b(7); 
     cout << "Node empty constructor: ";
     cout << a << endl;
     cout << "Node with value 7 constructor: ";
-    cout << b << endl;  
-    return 0;
+    cout << b << endl;
 }
-
-
-int main() {
+void LinkedListTest() {
     string a = "SecondLL"; // Second Linked List
+    string b = "FirstLL";
+    int values[3] = {0, 1, 2};
     LinkedList first;
     if (first.is_empty()) {
         cout << "Empty constructor works." << endl;
     }
     LinkedList second(a);
-    first.set_name("FirstLL");
+    first.set_name(a);
     cout << "Name of first linked list: " << first.get_name() << endl;
     cout << "Name of second linked list: " << second.get_name() << endl;
-    second.insert_front(0);
-    second.insert_back(2);
-    second.insert_after(0,2);
+    second.insert_front(values[0]);
+    second.insert_back(values[2]);
+    second.insert_after(values[0],values[1]);
+
     cout << second << endl;
+}
+
+int main() {
+    NodeTest();
+    LinkedListTest();
 }
